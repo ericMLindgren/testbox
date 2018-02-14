1e16ed1a4f7f69e24863277dff28247c13e3af0e
Eric Lindgren <nephilim.eric@gmail.com>
2018-02-14 02:54:45 -0800

    simplified since IO globbing is now handled by compilebox

--- src/challenges/challenge.go
+++ src/challenges/challenge.go
@@ -1,10 +1,5 @@
 package challenges
 
-import (
-	"log"
-	"strings"
-)
-
 type Id = string
 
 // Challenge - Defines challenge object
@@ -15,21 +10,35 @@ type Challenge struct {
 	SampleIO    string            `json:"sampleIO"`
 }
 
-func (c *Challenge) StringifyCases(sep string) (string, string) {
-	inputs := make([]string, len(c.IO))
-	outputs := make([]string, len(c.IO))
+func (c Challenge) GetIOSplit() ([]string, []string) {
+	ins := make([]string, len(c.IO))
+	outs := make([]string, len(c.IO))
 
-	log.Printf("getCases, challenge: %v\n", c)
-	i := 0
-	for k, v := range c.IO {
-		inputs[i] = k
-		outputs[i] = v
-		i++
+	var j int
+	for i, o := range c.IO {
+		ins[j] = i
+		outs[j] = o
+		j++
 	}
 
-	return joinAndAppend(inputs, sep), joinAndAppend(outputs, sep)
+	return ins, outs
 }
 
-func joinAndAppend(sl []string, endChar string) string {
-	return strings.Join(sl, endChar) + endChar
-}
+// func (c *Challenge) StringifyCases(sep string) (string, string) {
+// 	inputs := make([]string, len(c.IO))
+// 	outputs := make([]string, len(c.IO))
+
+// 	log.Printf("getCases, challenge: %v\n", c)
+// 	i := 0
+// 	for k, v := range c.IO {
+// 		inputs[i] = k
+// 		outputs[i] = v
+// 		i++
+// 	}
+
+// 	return joinAndAppend(inputs, sep), joinAndAppend(outputs, sep)
+// }
+
+// func joinAndAppend(sl []string, endChar string) string {
+// 	return strings.Join(sl, endChar) + endChar
+// }
