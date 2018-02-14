60cc1fc5cdd5566cda5511e28195c2b3b84efe6b
Eric Lindgren <nephilim.eric@gmail.com>
2018-02-14 02:57:33 -0800

    fully implemented test submission and grading added a couple of helper functions to improve readability throughout

--- src/testbox/main.go
+++ src/testbox/main.go
@@ -21,7 +21,7 @@ type CodeSubmission struct {
 	Language    string   `json:"language"`
 	Code        string   `json:"code"`
 	Stdins      []string `json:"stdins"`
-	ChallengeId string   `json:"challengeId`
+	ChallengeId string   `json:"challengeId,omitempty`
 }
 
 type ExecutionResult struct {
@@ -79,14 +79,6 @@ func main() {
 
 func getLangs(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received languages list request")
-	// langs := make(map[string]sandbox.Language)
-
-	// for k, v := range box.LanguageMap {
-	// 	langs[k] = sandbox.Language{Boilerplate: v.Boilerplate, CommentPrefix: v.CommentPrefix}
-	// }
-
-	// add boilerplate and comment info
-	// log.Println(langs)
 
 	buf, _ := json.MarshalIndent(languages, "", "   ")
 
@@ -106,61 +98,59 @@ func getStdout(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Received stdout-only code submission...")
 
 	// decode submission
-	decoder := json.NewDecoder(r.Body)
-	var submission CodeSubmission
-	err := decoder.Decode(&submission)
-	defer r.Body.Close()
-	if err != nil {
-		panic(err)
-	}
+	submission := decodeSubmission(r)
 
 	fmt.Printf("with stdin: %s\n", submission.Stdins[0])
+
 	// send to compilebox
 	result := execCodeSubmission(submission)
 
-	// encode result
-	buf, _ := json.MarshalIndent(result, "", "   ")
-
-	// write result to client
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(buf)
-}
-
-func execCodeSubmission(s CodeSubmission) ExecutionResult {
-	// encode submission
-	jsonBytes, _ := json.MarshalIndent(s, "", "   ")
-	buf := bytes.NewBuffer(jsonBytes)
-
-	// send code to compilebox
-	r, err := http.Post(cbAddress+"/eval/", "application/json", buf)
-	if err != nil {
-		panic(err)
-	}
-
-	// decode response
-	decoder := json.NewDecoder(r.Body)
-	var result ExecutionResult
-	err = decoder.Decode(&result)
-	defer r.Body.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	// return result
-	return result
+	// encode and write result back to client
+	jsonWrite(result, w)
 }
 
 func submitToChallenge(w http.ResponseWriter, r *http.Request) {
-	log.Print("Received code submission for challenge...")
-	decoder := json.NewDecoder(r.Body)
-	var submission CodeSubmission
-	err := decoder.Decode(&submission)
-	if err != nil {
-		panic(err)
-	}
-	defer r.Body.Close()
+	fmt.Print("Received code submission for challenge...")
 
-	log.Printf("Code targets challenge %s", submission.ChallengeId)
+	submission := decodeSubmission(r)
+
+	fmt.Printf("Code targets challenge %s", submission.ChallengeId)
+	fmt.Println(submission)
+
+	// attach the appropriate challenge's stdins to the submission
+	stdins, expectedStdouts := challenges.GetById(submission.ChallengeId).GetIOSplit()
+	submission.Stdins = stdins
+
+	// send to compilebox
+	result := execCodeSubmission(submission)
+
+	// compare stdouts to challenges stdouts
+	fmt.Printf("about to grade, expecting %v\n", expectedStdouts)
+	result.Graded = gradeResults(stdins, expectedStdouts, result.Stdouts)
+
+	// encode and write result back to client
+	jsonWrite(result, w)
+}
+
+// TODO consider that grading in strings should be done client side and that testbox side it should be bools?
+func gradeResults(ins, exp, actual []string) map[string]string {
+	graded := make(map[string]string)
+	for i, e := range exp {
+		// if we've run out of results, fail. This should never happen but would cause index error if it did
+		// if i > len(actual)-1 {
+		// 	graded[e] = "Fail"
+		// 	continue
+		// }
+		thisIn := ins[i]
+
+		if e != actual[i] {
+			graded[thisIn] = "Fail"
+			continue
+		}
+
+		graded[thisIn] = "Pass"
+	}
+	return graded
 }
 
 func populateLanguages() {
@@ -187,6 +177,50 @@ func populateLanguages() {
 		supportedLangs = append(supportedLangs, fmt.Sprintf("%s", k))
 	}
 	fmt.Printf("Supporting: %s\n", strings.Join(supportedLangs, ", "))
+}
+
+func execCodeSubmission(s CodeSubmission) ExecutionResult {
+	// encode submission
+	jsonBytes, _ := json.MarshalIndent(s, "", "   ")
+	buf := bytes.NewBuffer(jsonBytes)
+
+	// send code to compilebox
+	r, err := http.Post(cbAddress+"/eval/", "application/json", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	// decode response
+	decoder := json.NewDecoder(r.Body)
+	var result ExecutionResult
+	err = decoder.Decode(&result)
+	defer r.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	// return result
+	return result
+}
+
+func jsonWrite(v interface{}, w http.ResponseWriter) {
+	// encode variable
+	buf, _ := json.MarshalIndent(v, "", "   ")
+
+	// write encoded variable to client
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf)
+}
+
+func decodeSubmission(r *http.Request) CodeSubmission {
+	decoder := json.NewDecoder(r.Body)
+	var submission CodeSubmission
+	err := decoder.Decode(&submission)
+	defer r.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	return submission
 }
 
 func getEnv(key, fallback string) string {
